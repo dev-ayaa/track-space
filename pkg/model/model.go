7cb536fd1aa49b90a4f683873dad1a1f1962301c
dev-ayaa <ayaaakinleye@gmail.com>
2022-07-14 14:51:33 +0100

    update the database model

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,34 +2,41 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	FirstName      string
-	LastName       string
-	Email          string
-	Password       string
-	YrsOfExp       int32
-	Location       string
-	PhoneNumber    string
-	IPAddress      string
-	UserType       []string
-	Stack          []string
-	ProjectDetails []Project
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID             primitive.ObjectID `json:"_id" bson:"_id"`
+	FirstName      *string            `json:"first_name" validate:"required"`
+	LastName       *string            `json:"last_name" validate:"required"`
+	Email          *string            `json:"email" validate:"required"`
+	Password       *string            `json:"password" validate:"required"`
+	YrsOfExp       *int32             `json:"yrs_of_exp"`
+	Location       *string            `json:"location" validate:"required"`
+	PhoneNumber    *string            `json:"phone_number" validate:"required"`
+	IPAddress      *string            `json:"ip_address"`
+	UserType       []string           `json:"user_type"`
+	Stack          []string           `json:"stack"`
+	ProjectDetails []Project          `json:"project_details" bson:"project_details"`
+	CreatedAt      time.Time          `json:"created_at"`
+	UpdatedAt      time.Time          `json:"updated_at"`
 }
 
 type Project struct {
-	ProjectName string
-	ToolsUse    []string
-	Duration    time.Duration
-	ProjectTask string
+	ID          primitive.ObjectID `bson:"_id"`
+	ProjectName *string            `json:"project_name"`
+	ToolsUse    []string           `json:"tools_use"`
+	StartTime   time.Time          `json:"start_time"`
+	EndTime     time.Time          `json:"end_time"`
+	Duration    time.Duration      `json:"duration"`
+	ProjectTask *string            `json:"project_tasks"`
 }
 
 type Email struct {
-	Message      string
-	Receiver     string
-	Sender       string
-	MailTemplate string
+	ID           primitive.ObjectID `json:"_id" bson:"_id"`
+	Message      *string            `json:"message"`
+	Receiver     *string            `json:"receiver" validate:"required"`
+	Sender       *string            `json:"sender" validate:"required"`
+	MailTemplate *string            `json:"mail_template"`
 }
