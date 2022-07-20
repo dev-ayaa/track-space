1d83295b566129edcea2b9e622dda9c7aa4eb3eb
dev-ayaa <ayaaakinleye@gmail.com>
2022-07-20 14:27:13 +0100

    load the env value for database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"context"
+	"github.com/gin-contrib/sessions/cookie"
 	"html/template"
 	"log"
 	"os"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/yusuf/track-space/pkg/config"
+	"github.com/yusuf/track-space/pkg/controller"
 	"github.com/yusuf/track-space/pkg/data"
-	"github.com/yusuf/track-space/pkg/dbdriver"
-	"github.com/yusuf/track-space/pkg/handler"
+	"github.com/yusuf/track-space/pkg/db"
 )
 
 var app config.AppConfig
 
 func main() {
 
+	/*Application configuration*/
 	app.AppInProduction = false
 	app.UseTempCache = false
-
-	var Client = dbdriver.DatabaseConnection()
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("No .env file available")
 	}
 
+	/* Get the URI string from the ENV*/
+	mongodbUri := os.Getenv("MONGODBURI")
+	if mongodbUri == "" {
+		log.Println("mongodb cluster uri not found : ")
+	}
+
+	/*Connecting to the database*/
+	var Client = db.DatabaseConnection(mongodbUri)
+
 	user := data.UserData(Client, "user")
 	//mail = data.MailData(Client, "mail")
 
+	/*Disconnect the database using defer -> FIRST-IN-LAST-OUT*/
 	defer func() {
 		if err = Client.Disconnect(context.TODO()); err != nil {
 			log.Fatal(err)
@@ -38,8 +49,9 @@ func main() {
 		}
 	}()
 
-	repo := handler.NewAppHandler(&app, user)
+	repo := controller.NewAppHandler(&app, user)
 
+	/* Get the port address string from the ENV file */
 	portNumber := os.Getenv("PORTNUMBER")
 	if portNumber == "" {
 		log.Println("No local server port number created!")
@@ -56,12 +68,17 @@ func main() {
 	}
 
 	appRouter.Use(gin.Logger(), gin.Recovery())
+	keyWare := cookie.NewStore([]byte("appKey"))
+	appRouter.Use(sessions.Sessions("session", keyWare))
 
 	//this is to verify a list of the html/tmpl
 	//and render the correct template with it templates data
 	appRouter.SetFuncMap(template.FuncMap{})
 
-	appRouter.LoadHTMLGlob("templates/*")
+	appRouter.Static("/static", "./static")
+	//appRouter.StaticFS("/static", http.Dir("track-space"))
+	//appRouter.StaticFile("/favicon.ico", "./resources/favicon.ico")
+	appRouter.LoadHTMLGlob("templates/*.tmpl")
 
 	Routes(appRouter, *repo)
 
