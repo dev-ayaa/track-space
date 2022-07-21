023dd2c5afb7cc14270817ac6a3b45d22951680a
dev-ayaa <ayaaakinleye@gmail.com>
2022-07-21 10:08:52 +0100

    renamed the database

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -7,13 +7,13 @@ import (
 // UserData Setting up the database for the user data collection
 func UserData(dbClient *mongo.Client, collectionName string) *mongo.Collection {
 	var userCollection *mongo.Collection
-	userCollection = dbClient.Database("track-space").Collection(collectionName)
+	userCollection = dbClient.Database("track_space").Collection(collectionName)
 	return userCollection
 }
 
 // MailData Setting up the database for the mail data collection
 func MailData(dbClient *mongo.Client, collectionName string) *mongo.Collection {
 	var mailCollection *mongo.Collection
-	mailCollection = dbClient.Database("track-space").Collection(collectionName)
+	mailCollection = dbClient.Database("track_space").Collection(collectionName)
 	return mailCollection
 }
