0ea73c3fe4c63f9eb865112a154f771e89a249da
dev-ayaa <ayaaakinleye@gmail.com>
2022-07-21 10:25:26 +0100

    refactor code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	// "net/http"
+
 
 	"github.com/gin-gonic/gin"
-	// "github.com/go-chi/chi"
 
 	"github.com/yusuf/track-space/pkg/controller"
-	// "github.com/yusuf/track-space/pkg/config"
-	// "github.com/yusuf/track-space/pkg/controller"
+	
 )
 
 func Routes(routes *gin.Engine, h controller.AppHandler) {
