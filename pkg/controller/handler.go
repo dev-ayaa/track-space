cad554df6c91f4daad87bd4489803536d04ca02d
dev-ayaa <ayaaakinleye@gmail.com>
2022-07-21 10:14:47 +0100

    redirect and getting the user detail using session

--- pkg/controller/handler.go
+++ pkg/controller/handler.go
@@ -1,12 +1,22 @@
 package controller
 
 import (
+	"context"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/yusuf/track-space/pkg/config"
+	"github.com/yusuf/track-space/pkg/key"
+	"github.com/yusuf/track-space/pkg/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"net/http"
+	"time"
 )
+
+var Validate = validator.New()
 
 // AppHandler Implement the repository pattern to access multiple package all at once
 // this will give me access to the app configuration package
@@ -28,31 +38,67 @@ func NewAppHandler(appConfig *config.AppConfig, userCol *mongo.Collection) *AppH
 // HomePage controller to display the home page of the application
 func (ah *AppHandler) HomePage() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		c.HTML(http.StatusOK, "home.page.tmpl", gin.H{})
 	}
 }
 
 func (ah AppHandler) SignUpPage() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.page.tmpl", gin.H{})
+		c.HTML(http.StatusOK, "signup.page.tmpl", gin.H{})
 	}
 }
 
 func (ah AppHandler) PostSignUpPage() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user model.User
+
+		if err := Validate.Struct(&user); err != nil {
+			_ = c.AbortWithError(http.StatusBadRequest, gin.Error{Err: err})
+			return
+		}
+
+		//Parse the form and check for an err
 		err := c.Request.ParseForm()
 		if err != nil {
 			log.Println(err)
 		}
-		_ = c.Request.Form.Get("email")
-		_ = c.Request.Form.Get("password")
+
+		user.ID = primitive.NewObjectID()
+		user.Email = c.Request.Form.Get("email")
+		user.Password = key.HashPassword(c.Request.Form.Get("password"))
+
+		scs := sessions.Default(c)
+		scs.Set("email", user.Email)
+		scs.Set("password", user.Password)
+		err = scs.Save()
+		if err != nil {
+			log.Println(err)
+		}
+
+		ctx, cancelCtx := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancelCtx()
+
+		signUpDoc := bson.D{
+			{"email", user.Email},
+			{"password", user.Password},
+		}
+		_, err = ah.UserCol.InsertOne(ctx, signUpDoc)
+		if err != nil {
+			log.Println("cannot insert user sign up details in the database")
+			_ = c.AbortWithError(http.StatusInternalServerError, gin.Error{Err: err})
+			return
+		}
+
+		c.Redirect(http.StatusSeeOther, "/user-info")
 	}
 }
 
 func (ah *AppHandler) GetUserInfo() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "user-info.page.tmpl", gin.H{})
+		scs := sessions.Default(c)
+
+		c.JSON(http.StatusOK, gin.H{"email": scs.Get("email"), "password": scs.Get("password")})
+		//c.HTML(http.StatusOK, "user-info.page.tmpl", gin.H{})
 	}
 }
 
